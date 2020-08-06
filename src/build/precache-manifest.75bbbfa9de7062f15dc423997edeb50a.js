523e11fe73899b414476068dbfd03eef6c3600df
halmanza <anthonya771@gmail.com>
2020-08-06 00:01:36 -0700

    initial commit

---
+++ src/build/precache-manifest.75bbbfa9de7062f15dc423997edeb50a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5ae7bc83be347b9bfc936918808b9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95cbb6b65432806f4ec3",
+    "url": "/static/css/main.d145473b.chunk.css"
+  },
+  {
+    "revision": "1c29e2e1493d4879ea93",
+    "url": "/static/js/2.7adf56d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7adf56d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95cbb6b65432806f4ec3",
+    "url": "/static/js/main.07a803ad.chunk.js"
+  },
+  {
+    "revision": "13ff3f6c52d527a3b5c1",
+    "url": "/static/js/runtime-main.cf4e4ba0.js"
+  }
+]);
